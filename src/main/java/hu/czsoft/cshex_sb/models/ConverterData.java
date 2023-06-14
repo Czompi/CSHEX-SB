c544e6dee7b023a309cb4542596453dcdf38ccce
Dávid Czompó <czompi@czompi.hu>
2023-06-14 19:58:59 +0200

    Made ConverterData JSON output snake_case

--- src/main/java/hu/czsoft/cshex_sb/models/ConverterData.java
+++ src/main/java/hu/czsoft/cshex_sb/models/ConverterData.java
@@ -1,5 +1,9 @@
 package hu.czsoft.cshex_sb.models;
 
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class ConverterData {
     public String url;
 }
